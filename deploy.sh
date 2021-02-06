cbb442031fcc2657e35fd1baee4517900d98c7b4
penta-dev-1 <m.soliman@penta-b.com>
2021-02-06 03:28:54 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-aws ecr describe-repositories --repository-names $CIRCLE_PROJECT_REPONAME || aws ecr create-repository --repository-name $CIRCLE_PROJECT_REPONAME
 ecs-cli configure profile --profile-name $profile --access-key $accesskey --secret-key $secretkey
 ecs-cli configure --cluster $cluster_name --default-launch-type $launch_type --region $region --config-name $config_name 
 ecs-cli compose up  --cluster-config $config_name --ecs-profile $profile 
